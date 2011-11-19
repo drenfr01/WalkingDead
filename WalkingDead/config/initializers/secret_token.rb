0e66d493d1001106508c2f0b118a5dd332d744c6
Duncan Renfrow-Symon <duncanrenfrow@gmail.com>
2011-11-19 16:58:03 -0500

    Fixing naming bugs

--- WalkingDead/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-WalkingDead::Application.config.secret_token = '88cd9edde166b4c2737919a3b5ab993a43f26a63195e72d00c86c92b23d3cf0baa3d5d3bfe256e72696ba037752885422f0885e2a9aba1173f0357dc8e97ede5'
