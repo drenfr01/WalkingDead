2c644ce29224ff2b79ba6afb39f2e7d8967fc530
unknown <Andrew@Andrew-PC.(none)>
2011-11-28 17:20:37 -0500

    recreate rails app

---
+++ challenge/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Challenge::Application.config.secret_token = 'c1ceed34da1282f5dadfcae551196c1eba998857e82defd241b7c905ab9ee05ea441a7818081f05a058b7a2bb66d909fadbe6b75667a407cec1ebbeb28df03ba'
