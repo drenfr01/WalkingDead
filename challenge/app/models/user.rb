6ed6540b4b5072400a1473426d8c4f0e1e9f2382
Andrew Paseltiner <andrew.paseltiner@tufts.edu>
2011-12-02 14:40:53 -0500

    add validation to User model

--- challenge/app/models/user.rb
+++ challenge/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	validates :username, :presence => true, :uniqueness => true
+	validates :password, :presence => true, :confirmation => true
+	validates :password_confirmation, :presence => true
+	validates :lat, :numericality => true
+	validates :lng, :numericality => true
 end
