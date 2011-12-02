10101a213b8f8cb14080eb0e35469a6a93168026
Andrew Paseltiner <andrew.paseltiner@tufts.edu>
2011-12-02 14:35:51 -0500

    destroy GeoChallenge model

--- challenge/app/models/geo_challenge.rb
+++
@@ -1,7 +0,0 @@
-class GeoChallenge < ActiveRecord::Base
-	validates_presence_of :user, :goal, :completed
-	validates_associated :user, :goal
-
-	belongs_to :user
-	has_one :goal, :class_name => "Waypoint"
-end
