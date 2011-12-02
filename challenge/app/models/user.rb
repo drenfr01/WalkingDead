decace803933c57b1b8c821e529fe15bcd053660
Andrew Paseltiner <andrew.paseltiner@tufts.edu>
2011-12-02 14:59:00 -0500

    allow blank lat/lng in User model

--- challenge/app/models/user.rb
+++ challenge/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	validates :username, :presence => true, :uniqueness => true
 	validates :password, :presence => true, :confirmation => true
 	validates :password_confirmation, :presence => true
-	validates :lat, :numericality => true
-	validates :lng, :numericality => true
+	validates :lat, :numericality => true, :allow_blank => true
+	validates :lng, :numericality => true, :allow_blank => true
 end
