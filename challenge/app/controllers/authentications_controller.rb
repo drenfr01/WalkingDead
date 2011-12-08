e18d8767f4a9fe8de447ffdc014ef8834f6f16e1
Duncan Renfrow-Symon <duncanrenfrow@gmail.com>
2011-12-08 00:44:29 -0500

    Awesome routing done

--- challenge/app/controllers/authentications_controller.rb
+++ challenge/app/controllers/authentications_controller.rb
@@ -8,12 +8,14 @@ class AuthenticationsController < ApplicationController
         authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
  	if authentication
 		flash[:notice] = "Signed in successfully"
-		sign_in_and_redirect(:user, authentication.user)
+		sign_in(:user, authentication.user)
+		redirect_to "/mobile/loglocation/"
 	elsif current_user
 		current_user.authentications.create(:provider => omniauth['provider'], 
 		:uid => omniauth['uid'])
     		flash[:notice] = "Authentication successfull"
-     		redirect_to authentications_url  
+     		# redirect_to authentications_url  
+     		redirect_to "/mobile/loglocation/"
   	else 
 		user = User.new
 		user.authentications.build(:provider => omniauth['provider'],
